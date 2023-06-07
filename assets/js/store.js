94bafbb271e7d5854b68844fded51024f9b3dc54
Lucas Hiago <lucashiago63@gmail.com>
2023-06-07 17:44:03 -0300

    feat: add redux-thunk

--- assets/js/store.js
+++ assets/js/store.js
@@ -1,5 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reducers from './reducers/Index';
 
-const store = createStore(reducers);
+// Enable async/await with redux-thunk
+const composedEnhancer = applyMiddleware(thunk);
+
+const store = createStore(reducers, composedEnhancer);
 export default store;
