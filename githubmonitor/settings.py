a0452d74e40a8f9f1c71d23bb40781d1c32b0088
Lucas Hiago <lucashiago63@gmail.com>
2023-06-09 09:23:18 -0300

    chore: add django-filters

--- githubmonitor/settings.py
+++ githubmonitor/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'social_django',
     'webpack_loader',
     'django_extensions',
@@ -143,6 +144,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
